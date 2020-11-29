3f2c3f9c9f05258b877e706d09fb8f814adb2a0e
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2020-11-29 09:44:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,49 @@
-import 'package:flutter/material.dart';import 'icons.dart';void main() => runApp(MyApp());class MyApp extends StatelessWidget {  @override  Widget build(BuildContext context) {    return MaterialApp(      debugShowCheckedModeBanner: false,      home: IconDemo(),    );  }}class IconDemo extends StatelessWidget {  @override  Widget build(BuildContext context) {    return Scaffold(      appBar: AppBar(        title: Text('Icons'),      ),      body: GridView.builder(        itemCount: icons.length,          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(            crossAxisCount: 2,          ),          itemBuilder: (context, int index){          return Icon(icons[index],size: 50,);          }      ),    );  }}
+import 'package:flutter/material.dart';
+import 'icons.dart';
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: IconDemo(),
+    );
+  }
+}
+
+class IconDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Icons'),
+      ),
+      body: GridView.builder(
+        itemCount: icons.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          itemBuilder: (context, int index){
+          return Icon(icons[index],size: 50,);
+          }
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

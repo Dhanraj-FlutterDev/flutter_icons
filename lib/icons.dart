c01e225473459ddeaaea6b5de544b1457fa3e5cf
Dhanraj-MobileAppDev <52417093+DhanrajNilkanth@users.noreply.github.com>
2021-02-15 12:18:03 +0530

    Update icons.dart

--- lib/icons.dart
+++ lib/icons.dart
@@ -1,1 +1,35 @@
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';List icons = [  FontAwesomeIcons.accessibleIcon,  FontAwesomeIcons.google,  FontAwesomeIcons.facebook,  FontAwesomeIcons.apple,  FontAwesomeIcons.microsoft,  FontAwesomeIcons.windowRestore,  FontAwesomeIcons.sdCard,  FontAwesomeIcons.eraser,  FontAwesomeIcons.asterisk,  FontAwesomeIcons.objectGroup,  FontAwesomeIcons.link,  FontAwesomeIcons.qrcode,  FontAwesomeIcons.questionCircle,  FontAwesomeIcons.xbox,  FontAwesomeIcons.gamepad,  FontAwesomeIcons.footballBall,  FontAwesomeIcons.landmark,  FontAwesomeIcons.signLanguage,  FontAwesomeIcons.amazon,  FontAwesomeIcons.ambulance,  FontAwesomeIcons.eject,  FontAwesomeIcons.calculator,  FontAwesomeIcons.youtube,  FontAwesomeIcons.amazonPay,];
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+
+
+List icons = [
+
+  FontAwesomeIcons.accessibleIcon,
+  FontAwesomeIcons.google,
+  FontAwesomeIcons.facebook,
+  FontAwesomeIcons.apple,
+  FontAwesomeIcons.microsoft,
+  FontAwesomeIcons.windowRestore,
+  FontAwesomeIcons.sdCard,
+  FontAwesomeIcons.eraser,
+  FontAwesomeIcons.asterisk,
+  FontAwesomeIcons.objectGroup,
+  FontAwesomeIcons.link,
+  FontAwesomeIcons.qrcode,
+  FontAwesomeIcons.questionCircle,
+  FontAwesomeIcons.xbox,
+  FontAwesomeIcons.gamepad,
+  FontAwesomeIcons.footballBall,
+  FontAwesomeIcons.landmark,
+  FontAwesomeIcons.signLanguage,
+  FontAwesomeIcons.amazon,
+  FontAwesomeIcons.ambulance,
+  FontAwesomeIcons.eject,
+  FontAwesomeIcons.calculator,
+  FontAwesomeIcons.youtube,
+  FontAwesomeIcons.amazonPay,
+];
+
+
+
